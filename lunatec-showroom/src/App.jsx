0f402e4d80e3a83cad807c37039a7cc35860d0fc
nehuenaguirre <nehuenaguirre1999@gmail.com>
2026-04-18 04:14:42 -0300

    Add files via upload

--- lunatec-showroom/src/App.jsx
+++ lunatec-showroom/src/App.jsx
@@ -1,19 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import { CartProvider } from './context/CartContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import CartSidebar from './components/CartSidebar';
+import { trackEvent } from './utils/analytics'; // <-- IMPORTAMOS LA ANALÍTICA
 
-// Páginas (Las crearemos en la siguiente parte)
+// Páginas
 import Home from './pages/Home';
 import CategoryView from './pages/CategoryView';
 import ProductDetail from './pages/ProductDetail';
 import CartFull from './pages/CartFull';
 
+// Este componente "invisible" rastrea cada ventana que visita el usuario
+function RouteTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Registra la vista de página con la URL exacta (ej: '/' o '/categoria/auriculares')
+    trackEvent('page_view', location.pathname);
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <CartProvider>
       <BrowserRouter>
+        <RouteTracker /> {/* <-- ACTIVAMOS EL RASTREADOR */}
         <div className="flex flex-col min-h-screen font-sans bg-gray-50">
           <Navbar />
           <CartSidebar />
